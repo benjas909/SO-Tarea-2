39be2e68824fcaa4fa463a3f96dcf18c5b3187d8
benjas <benjamin.aguilera64@gmail.com>
2024-04-29 23:11:12 -0400

    Agrega primeras variables

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <unistd.h>
+using namespace std;
+
+int main()
+{
+  int ATK, DEF, HP, EVA;
+  pid_t playerPID;
+  pid_t wrestlerPID1;
+  pid_t wrestlerPID2;
+  pid_t wrestlerPID3;
+
+  return 0;
+}
